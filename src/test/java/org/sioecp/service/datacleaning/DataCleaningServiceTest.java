07e468a9f49a0a80b7339a0f81e6d681484b1340
marin <marin>
2017-03-06 23:42:44 +0100

    Finish test for Weather Cleaning

--- src/test/java/org/sioecp/service/datacleaning/DataCleaningServiceTest.java
+++ src/test/java/org/sioecp/service/datacleaning/DataCleaningServiceTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.sioecp.service.datacleaning.tools.SqlConnector;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Test Weather Data Cleaning")
@@ -12,9 +14,26 @@ class DataCleaningServiceTest {
     @Test
     void TestWeatherCleaning(){
         SqlConnector sql = new SqlConnector();
-        sql.importPropertiesFromFile("config.properties");
+        sql.importPropertiesFromFile("config-test.properties");
 
-        DataCleaningService cleaner = new DataCleaningService("config.properties");
+        // Count DW_weather rows
+        int warehouseWeatherRows = sql.execCount("DW_weather",null);
+        assertEquals(0,warehouseWeatherRows);
+
+        // Exec cleaning service
+        DataCleaningService cleaner = new DataCleaningService("config-test.properties");
         cleaner.cleanWeather();
+
+        // Count DW_weather rows
+        warehouseWeatherRows = sql.execCount("DW_weather",null);
+        assertEquals(3,warehouseWeatherRows);
+
+        // Exec cleaning service again: nothing more should be cleaned
+        cleaner = new DataCleaningService("config-test.properties");
+        cleaner.cleanWeather();
+
+        // Count DW_weather rows
+        warehouseWeatherRows = sql.execCount("DW_weather",null);
+        assertEquals(3,warehouseWeatherRows);
     }
 }
